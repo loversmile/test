31d77d1c01a6b927df373cb76e3a83c4900c47d0
jklou <jklou@grandstream.com>
2016-01-15 09:21:15 +0800

    [test] Cause: Solution: Fixed Version: 1.0 Branch: master

---
+++ test2.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test2.c
+	> Author: jklou
+	> Mail: 
+	> Created Time: 2016年01月15日 星期五 09时17分51秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    printf("Haha, this for a test !!!!!\n");
+}
